4d82d9bfb02ab07f08ec7171ad714040fe40e721
pkundu307 <pkundu307@gmail.com>
2025-07-03 22:03:57 +0530

    chat implemented without chat history

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -8,7 +8,25 @@ const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
+ const fetchUserId = async (token:string) => {
 
+  if (!token) return null;
+
+  try {
+    const res = await api.get('/user/me', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const userId = res.data._id;
+    localStorage.setItem('userId', userId);
+    return userId;
+  } catch (err) {
+    console.error('Failed to fetch user:', err);
+    return null;
+  }
+};
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();// if we don't prevent default, the page will reload
     if (!email || !password) {
@@ -19,6 +37,7 @@ const Login = () => {
       const res = await api.post('/auth/login', { email, password });
       localStorage.setItem('token', res.data.token);
       localStorage.setItem('loginTime', new Date().toISOString());
+      await fetchUserId(res.data.token);
       navigate('/home');
       //refresh the page to show the new token
         window.location.reload();
