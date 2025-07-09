1c83ba0cc183374e1e96bebecd94357c1321b3b3
Your Name <ramavathsrikanth20@gmail.com>
2025-07-09 16:59:03 +0530

    Completed task 8: Added post delete functionality

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,30 @@ import Friends from "./pages/Friends";
 import FriendProfile from "./pages/FriendProfile";
 import ChatPopUp from "./components/ChatPopUp";
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
-    
   const token = localStorage.getItem("token");
-  console.log(token?.length);
 
   return (
     <>
-     
       <BrowserRouter>
-      {token &&  <Navbar />}
+        {token && <Navbar />}
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/home" element={<Home />} />
-          <Route path='/friends' element={<Friends/>}/>
+          <Route path="/friends" element={<Friends />} />
           <Route path="/profile/:id" element={<FriendProfile />} />
         </Routes>
       </BrowserRouter>
-      <ChatPopUp/>
+
+      {/* Always shown on screen */}
+      <ChatPopUp />
+
+      {/* ✅ This line is step 2: REQUIRED to display toast messages */}
+      <ToastContainer position="top-right" autoClose={3000} pauseOnHover />
     </>
   );
 }
